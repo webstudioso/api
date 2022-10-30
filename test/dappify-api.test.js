4c0afeb3b18324d6f47b75d0f347f3de97ea9a9e
Giancarlo Sanchez <gianksp@gmail.com>
2022-10-30 10:04:22 +0000

    Initial commit

---
+++ test/dappify-api.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const DappifyApi = require('../lib/dappify-api-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dappify-api-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new DappifyApi.DappifyApiStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
