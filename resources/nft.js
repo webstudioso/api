cee78516c99621ec2b17ddd979dae78965d9a0ac
Giancarlo Sanchez <gianksp@gmail.com>
2022-11-01 09:35:41 +0000

    Decoupling ABI definitions

--- resources/nft.js
+++ resources/nft.js
@@ -11,6 +11,11 @@ https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-handler.html
 const AWS = require('aws-sdk');
 const ethers = require('ethers');
 const axios = require('axios');
+
+const ERC165ABI = require('./abi/ERC165.json');
+const ERC721ABI = require('./abi/ERC721.json');
+const ERC1155ABI = require('./abi/ERC1155.json');
+
 const S3 = new AWS.S3();
 
 const bucketName = process.env.BUCKET;
@@ -22,94 +27,11 @@ const url = 'https://eth-rpc-mandala.aca-staging.network/';
 let customHttpProvider = new ethers.providers.JsonRpcProvider(url);
 let privateKey = '0x3ab6468f2465130c51946a5456b8e2d309be7af2f8afcd6823996d281c0990d0';
 
-const abi = [
-  {
-    inputs: [],
-    name: "name",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "bytes4", name: "interfaceId", type: "bytes4" }],
-    name: "supportsInterface",
-    outputs: [{ internalType: "bool", name: "", type: "bool" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "symbol",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [{ internalType: "uint256", name: "tokenId", type: "uint256" }],
-    name: "tokenURI",
-    outputs: [{ internalType: "string", name: "", type: "string" }],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-		inputs: [
-			{
-				internalType: "uint256",
-				name: "tokenId",
-				type: "uint256"
-			}
-		],
-		name: "uri",
-		outputs: [
-			{
-				internalType: "string",
-				name: "",
-				type: "string"
-			}
-		],
-		stateMutability: "view",
-		type: "function"
-	},
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "tokenId",
-        type: "uint256"
-      }
-    ],
-    name: "ownerOf",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    stateMutability: "view",
-    type: "function"
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address"
-      }
-    ],
-    stateMutability: "view",
-    type: "function"
-  }
-];
-
-
-
 
 exports.main = async function(event, context) {
   try {
-    var method = event.httpMethod;
+    // var method = event.httpMethod;
+    let contract;
 
     // if (method === "GET") {
     //   if (event.path === "/") {
@@ -129,22 +51,26 @@ exports.main = async function(event, context) {
     const tokenId = event['pathParameters']['token_id'];
 
     const signer = new ethers.Wallet(privateKey, customHttpProvider);
-    const contract = new ethers.Contract(address,abi,signer);
+    contract = new ethers.Contract(address, ERC165ABI, signer);
 
     const IERC721 = '0x80ac58cd';
-    const IERC1155 = '0xd9b67a26';
+    // const IERC1155 = '0xd9b67a26';
     const isERC721 = await contract.supportsInterface(IERC721);
     // const isERC1155 = await contract.supportsInterface(IERC1155);
-    let tokenUri, ownerQuery;
+    let tokenUri, ownerQuery, amountQuery;
     // if (isERC1155) {
     //   tokenUri = await contract.uri(tokenId);
     if (isERC721) {
+      contract = new ethers.Contract(address, ERC721ABI, signer);
       tokenUri = await contract.tokenURI(tokenId);
       ownerQuery = contract.ownerOf(tokenId);
+      amountQuery = new Promise((resolve) => resolve(1));
     } else {
       // Not supported
+      contract = new ethers.Contract(address, ERC1155ABI, signer);
       tokenUri = await contract.uri(tokenId);
       ownerQuery = contract.owner();
+      amountQuery = contract.balanceOf(address, tokenId);
     }
 //   console.log('DOS');
 //   // const tokenURI = await contract.tokenURI(tokenId);
@@ -152,7 +78,7 @@ exports.main = async function(event, context) {
   const symbolQuery = contract.symbol();
   const metadataQuery = axios.get(tokenUri);
 
-  const queries = await Promise.all([nameQuery, symbolQuery, metadataQuery, ownerQuery]);
+  const queries = await Promise.all([nameQuery, symbolQuery, metadataQuery, ownerQuery, amountQuery]);
   const nft = {
     token_id: tokenId,
     token_uri: tokenUri,
@@ -160,8 +86,9 @@ exports.main = async function(event, context) {
     symbol: queries[1],
     token_address: address,
     metadata: queries[2]?.data,
-    owner: queries[3],
-    type: isERC721 ? 'ERC721' : 'ERC1155'
+    owner_of: queries[3],
+    amount: queries[4],
+    contract_type: isERC721 ? 'ERC721' : 'ERC1155'
   }
 
 
