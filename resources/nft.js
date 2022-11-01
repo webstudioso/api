374df097cd404836aefa25b73eb02975ba9ee177
Giancarlo Sanchez <gianksp@gmail.com>
2022-11-01 08:28:02 +0000

    Definition for new functions nft

--- resources/nft.js
+++ resources/nft.js
@@ -69,7 +69,39 @@ const abi = [
 		],
 		stateMutability: "view",
 		type: "function"
-	}
+	},
+  {
+    inputs: [
+      {
+        internalType: "uint256",
+        name: "tokenId",
+        type: "uint256"
+      }
+    ],
+    name: "ownerOf",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  },
+  {
+    inputs: [],
+    name: "owner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address"
+      }
+    ],
+    stateMutability: "view",
+    type: "function"
+  }
 ];
 
 
